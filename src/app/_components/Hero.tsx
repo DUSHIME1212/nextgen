d2126e73a34d11f6d1b25a5bc0f82a108b3075ef
DUSHIME1212 <mdonavan33@gmail.com>
2024-12-20 23:06:09 +0200

    `Refactor Hero component and Navbar component with design updates`

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -5,9 +5,9 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="min-h-[720px] px-8 py-16 md:px-16 lg:px-32">
-      <div className="flex size-full min-h-[512px] flex-col items-start p-16 text-left justify-center text-white bg-blue-700">
+      <div className="flex size-full min-h-[512px] flex-col items-start p-16 text-left justify-center rounded-2xl text-white bg-gradient-to-br to-blue-500 from-blue-700">
         <div className="w-2/3">
-        <h3>
+        <h3 className="text-yellow-400">
           Providing Full-Stack Development, Web Design & Branding Solutions for
           an Effective Digital Experience
         </h3>
@@ -16,7 +16,7 @@ const Hero = () => {
           </p>
         </div>
         <div>
-          <Button variant={"gooeyRight"} className="bg-white text-blue-700 px-8 py-6 mt-8 rounded-lg">
+          <Button variant={"gooeyLeft"} className="bg-white from-blue-200 text-blue-700 px-8 py-6 mt-8 rounded-lg">
             Get Started
           </Button>
         </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,18 @@ import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button'
 import { navlinks } from '@/lib/utils'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <nav className='flex px-8 md:px-16 lg:px-32 w-full py-4 justify-between items-center'>
-        <Link href={""}>
-            Logo
+    <nav className='flex px-8 md:px-16 lg:px-32 w-full py-4 shadow-2xl shadow-blue-500/20 justify-between items-center'>
+        <Link href={""} className='flex items-center gap-2 text-2xl font-bold'>
+            <div className='size-8 relative'>
+                <Image src="/logo.png" alt="Logo" className='object-contain' fill/>
+            </div>
+            Nextgen
         </Link>
-        <div className='flex text-xl gap-4 items-center'>
+        <div className='flex gap-4 items-center'>
             {
                 navlinks.map((link, index) => (
                         <Link href={link.href} key={index} className='group-hover:text-blue-700 duration-500'>
@@ -17,7 +21,7 @@ const Navbar = () => {
                         </Link>
                 ))
             }
-        <Button size={"lg"} className='bg-blue-700 text-lg py-6'>Start customizing</Button>
+        <Button variant={"gooeyLeft"} className='bg-blue-700 text-white from-blue-100 py-6'>Start customizing</Button>
         </div>
     </nav>
   )
